ee9a30eb2809ed0cfa87f8dcf0ca60790808c966
Pranavkumar Mallela <pranavmallela2020@gmail.com>
2022-07-11 16:50:16 +0530

    deployment: backend and frontend working on same local server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
 require('dotenv').config();
 const manageRoutes = require('./routes/manageRoutes');
 const profileRoutes = require('./routes/profileRoutes');
@@ -8,19 +9,20 @@ const machineRoutes = require('./routes/machineRoutes');
 
 const app = express();
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static('../build'));
-    app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '../build/index.html'));
-    });
-   }
-
 //Middleware
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/manage', manageRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/machines', machineRoutes);
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static('../build/'));
+    app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../build/index.html'));
+    });
+   }
+
 mongoose.connect(process.env.MONGO_URI)
     .then(result => {
         app.listen(process.env.PORT,() => {
