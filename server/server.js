dbfd4c3558cb2d256c64f97cc66a5de374fe2360
Pranavkumar Mallela <pranavmallela2020@gmail.com>
2022-07-12 13:02:08 +0530

    deployment: changed server.js

--- server/server.js
+++ server/server.js
@@ -16,12 +16,12 @@ app.use('/api/manage', manageRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/machines', machineRoutes);
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static('../client/build/'));
-    app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../client/build/index.html'));
-    });
-   }
+// if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+//     app.use(express.static('../client/build/'));
+//     app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+//     });
+//    }
 
 mongoose.connect(process.env.MONGO_URI)
     .then(result => {
