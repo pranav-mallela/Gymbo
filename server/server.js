a4bc468971aa5a3873203041a2c6fe2e2b145786
Pranavkumar Mallela <pranavmallela2020@gmail.com>
2022-07-12 16:45:50 +0530

    heroku commit 6

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ app.use(morgan('dev'));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
     next();
 });
 app.use('/api/manage', manageRoutes);
