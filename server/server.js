8f55df8368882c44c2a5313245432fef7e907731
Pranavkumar Mallela <pranavmallela2020@gmail.com>
2022-07-12 12:30:13 +0530

    deployment: changed path to build file

--- server/server.js
+++ server/server.js
@@ -17,9 +17,9 @@ app.use('/api/profile', profileRoutes);
 app.use('/api/machines', machineRoutes);
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static('../build/'));
+    app.use(express.static('../client/build/'));
     app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../build/index.html'));
+    res.sendFile(path.join(__dirname + '/../client/build/index.html'));
     });
    }
 
